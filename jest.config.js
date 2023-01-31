41058f05f6bbd975e70b8ddbc7174d09ecb6e10b
Hans Fehrmann <hans.jfehrmann@gmail.com>
2023-01-31 10:39:50 -0800

    Added jest and run audit, and a dummy test

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  moduleFileExtensions: ['js', 'jsx'],
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tests/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/tests/__mocks__/styleMock.js',
+  },
+};
---
+++ tests/cnb/test.js
@@ -0,0 +1,7 @@
+import documentTransformer from "content_scripts/cnb/html_transformer"
+
+describe("Dummy test", () => {
+  it("will import cnb html transformer", () => {
+    expect(true).toBeTruthy()
+  });
+});
