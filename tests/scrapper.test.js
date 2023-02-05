f95008cc69a503a2ce7d1a7f1df39c805ef1a58a
Hans Fehrmann <hans.jfehrmann@gmail.com>
2023-02-05 15:07:48 -0800

    Added scrapper action test

---
+++ tests/scrapper.test.js
@@ -0,0 +1,107 @@
+/**
+ * @jest-environment jsdom
+ */
+
+// jsdom has a problem when setting jest-environment into jsdom ¯\_(ツ)_/¯
+// https://github.com/jsdom/whatwg-url/issues/209#issuecomment-1015559283
+import { TextEncoder, TextDecoder } from 'util';
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+import { processTransactions } from 'content_scripts/scrapper_actions'
+
+const dummyData = [
+  {
+    business: 'B1',
+    referenceNumber: '1',
+    transactionDate: 'D1',
+    currency: 'C1',
+    money: '12.34',
+  },
+  {
+    business: 'B2',
+    referenceNumber: '2',
+    transactionDate: 'D2',
+    currency: 'C2',
+    money: '12.34',
+  },
+  {
+    business: 'B3',
+    referenceNumber: '3',
+    transactionDate: 'D3',
+    currency: 'C3',
+    money: '12.34',
+  },
+  {
+    business: 'B4',
+    referenceNumber: '4',
+    transactionDate: 'D4',
+    currency: 'C4',
+    money: '12.34',
+  },
+]
+
+
+describe("Process transactions", () => {
+  describe("given empty local storage", () => {
+    it("should pass all information into clipboard", () => {
+      processTransactions(
+        null,
+        new Set(),
+        () => {
+          return new Promise(resolver => resolver(dummyData));
+        },
+        false,
+        (transactions) => {
+          expect(transactions).toEqual(
+            [
+              'D1\tB1\t\t\t\t12.34',
+              'D2\tB2\t\t\t\t12.34',
+              'D3\tB3\t\t\t\t12.34',
+              'D4\tB4\t\t\t\t12.34',
+            ].join('\n')
+          );
+          return new Promise(r => {});
+        },
+      );
+    });
+  });
+
+  describe("given some data in local storage", () => {
+    it("should pass all information into clipboard", () => {
+      processTransactions(
+        null,
+        new Set(['1', '3']),
+        () => {
+          return new Promise(resolver => resolver(dummyData));
+        },
+        false,
+        (transactions) => {
+          expect(transactions).toEqual(
+            [
+              'D2\tB2\t\t\t\t12.34',
+              'D4\tB4\t\t\t\t12.34',
+            ].join('\n')
+          );
+          return new Promise(r => {});
+        },
+      );
+    });
+  });
+
+  describe("given some data in local storage", () => {
+    it("should save information in local storage", () => {
+      processTransactions(
+        null,
+        new Set(['1', '3']),
+        () => {
+          return new Promise(resolver => resolver(dummyData));
+        },
+        false,
+        (transactions) => {
+          return new Promise(r => {});
+        },
+      );
+    });
+  });
+})
